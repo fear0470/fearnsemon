b5247557006fa996c6103a0f53f8b3b7a8b81545
Nicholas Fearns <you@example.com>
2020-04-09 00:52:33 +0000

    built first query to grab 9 pokemon

--- src/graphql/get-pokemons.js
+++ src/graphql/get-pokemons.js
@@ -1,5 +1,19 @@
 import gql from 'graphql-tag';
 
 export const GET_POKEMONS = gql`
-    query pokemons($first: Int!)
+    query pokemons($first: Int!) {
+        pokemons(first: $first) {
+            id
+            name
+            image
+            maxHP
+            maxCP
+            attacks{
+                special{
+                    name
+                    damage
+                }
+            }
+        }
+    }
 `
